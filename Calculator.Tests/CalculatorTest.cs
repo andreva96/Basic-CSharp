a5a1bbe6556dff0c896dd956ab40741057b1daa8
Andre Viegas <andre.viegas@farfetch.com>
2022-04-28 03:16:47 +0100

    Starting .Net Unit Testing Tutorial

---
+++ Calculator.Tests/CalculatorTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Calculator.Tests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.True(true);
+        }
+    }
+}
