2e4ca178fb251ddf2447b93c626c891569348d95
Andre Viegas <andre.viegas@farfetch.com>
2022-05-02 09:50:14 +0100

    Class Fixtures, Constructors and Disposable

--- XUnitExample.Tests/FiboTests.cs
+++ XUnitExample.Tests/FiboTests.cs
@@ -1,37 +1,69 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace XUnitExample.Tests
 {
-    public class FiboTests
+    public class FiboFixture : IDisposable
     {
+        public Fibo calc => new Fibo();
+
+        public void Dispose()
+        {
+            //clean
+        }
+    }
+
+    public class FiboTests : IClassFixture<FiboFixture>
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly MemoryStream memoryStream;
+        private readonly FiboFixture _fiboFixture;
+
+        public FiboTests(ITestOutputHelper testOutputHelper, FiboFixture fiboFixture)
+        {
+            _testOutputHelper = testOutputHelper;
+            _testOutputHelper.WriteLine("Constructor");
+            _fiboFixture = fiboFixture;
+
+            memoryStream = new MemoryStream();
+        }
+
         [Fact]
+        [Trait("Category","Fibo")]
         public void FiboDoesNotIncludeZero()
         {
-            var calc = new Fibo();
-            Assert.All(calc.FiboNumbers, n => Assert.NotEqual(0, n));
+            Assert.All(_fiboFixture.calc.FiboNumbers, n => Assert.NotEqual(0, n));
+            Assert.DoesNotContain(0, _fiboFixture.calc.FiboNumbers);
         }
 
         [Fact]
+        [Trait("Category", "Fibo")]
         public void FiboIncludes13()
         {
-            var calc = new Fibo();
-            Assert.Contains(13, calc.FiboNumbers);
+            Assert.Contains(13, _fiboFixture.calc.FiboNumbers);
         }
 
         [Fact]
+        [Trait("Category", "Fibo")]
         public void FiboDoesNotInclude4()
         {
-            var calc = new Fibo();
-            Assert.DoesNotContain(4, calc.FiboNumbers);
+            Assert.DoesNotContain(4, _fiboFixture.calc.FiboNumbers);
         }
 
         [Fact]
+        [Trait("Category", "Fibo")]
         public void CheckCollection()
         {
             var expectedCollection = new List<int>() { 1, 1, 2, 3, 5, 8, 13 };
-            var calc = new Fibo();
-            Assert.Equal(expectedCollection, calc.FiboNumbers);
+            Assert.Equal(expectedCollection, _fiboFixture.calc.FiboNumbers);
+        }
+
+        public void Dispose()
+        {
+            memoryStream.Dispose();
         }
     }
 }
