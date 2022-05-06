a6af9a370674b5472d7393d72e59238e1eee045c
Andre Viegas <andre.viegas@farfetch.com>
2022-05-06 17:30:36 +0100

    Adding local database settings and migration

--- CSharpCleanArchitecture/CleanArch.Mvc/Startup.cs
+++ CSharpCleanArchitecture/CleanArch.Mvc/Startup.cs
@@ -29,7 +29,7 @@ namespace CleanArch.Mvc
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("UniversityIndentityDBConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
